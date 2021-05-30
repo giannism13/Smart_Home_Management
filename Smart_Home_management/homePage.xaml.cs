37b569690224ee4a1102563d1c094247b997bd9e
Γιάννης Μικές <mikesgiannis@gmail.com>
2021-05-30 20:32:15 +0300

    Initial home page layout

---
+++ Smart_Home_management/homePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smart_Home_management {
+	/// <summary>
+	/// Interaction logic for homePage.xaml
+	/// </summary>
+	public partial class homePage : Page {
+		public homePage() {
+			InitializeComponent();
+		}
+	}
+}
