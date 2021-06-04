dcd37a93094d1db692a883afd329dcf07173261b
Γιάννης Μικές <mikesgiannis@gmail.com>
2021-06-04 13:47:17 +0300

    Intriduced boxes that appear when hovering the mouse on respective's room button

--- Smart_Home_management/homePage.xaml.cs
+++ Smart_Home_management/homePage.xaml.cs
@@ -23,47 +23,57 @@ namespace Smart_Home_management {
 		}
 
 		private void KitchenButton_MouseEnter(object sender, MouseEventArgs e) {
-			kitchen.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF4C70"));
+			kitchen.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF4C70");
+			KitchenBorder.Visibility = Visibility.Visible;
 		}
 
 		private void KitchenButton_MouseLeave(object sender, MouseEventArgs e) {
-			kitchen.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FE6584"));
+			kitchen.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FE6584");
+			KitchenBorder.Visibility = Visibility.Hidden;
 		}
 
 
 		private void LivingRoomButton_MouseEnter(object sender, MouseEventArgs e) {
-			livingRoom.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF4C70"));
+			livingRoom.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF4C70");
+			LivingRoomBorder.Visibility = Visibility.Visible;
 		}
 
 		private void LivingRoomButton_MouseLeave(object sender, MouseEventArgs e) {
-			livingRoom.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FE6584"));
+			livingRoom.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FE6584");
+			LivingRoomBorder.Visibility = Visibility.Hidden;
 		}
 
 
 		private void Bedroom1Button_MouseEnter(object sender, MouseEventArgs e) {
-			bedroom1.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF4C70"));
+			bedroom1.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF4C70");
+			Bedroom1Border.Visibility = Visibility.Visible;
 		}
 
 		private void Bedroom1Button_MouseLeave(object sender, MouseEventArgs e) {
-			bedroom1.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FE6584"));
+			bedroom1.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FE6584");
+			Bedroom1Border.Visibility = Visibility.Hidden;
 		}
 
 
 		private void Bedroom2Button_MouseEnter(object sender, MouseEventArgs e) {
-			bedroom2.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF4C70"));
+			bedroom2.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF4C70");
+			Bedroom2Border.Visibility = Visibility.Visible;
 		}
 
 		private void Bedroom2Button_MouseLeave(object sender, MouseEventArgs e) {
-			bedroom2.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FE6584"));
+			bedroom2.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FE6584");
+			Bedroom2Border.Visibility = Visibility.Hidden;
 		}
 
 
 		private void BathroomButton_MouseEnter(object sender, MouseEventArgs e) {
-			bathroom.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF4C70"));
+			bathroom.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF4C70");
+			BathroomBorder.Visibility = Visibility.Visible;
 		}
 
 		private void BathroomButton_MouseLeave(object sender, MouseEventArgs e) {
-			bathroom.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FE6584"));
+			bathroom.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FE6584");
+			BathroomBorder.Visibility = Visibility.Hidden;
 		}
 	}
 }
