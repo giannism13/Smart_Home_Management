1979ba44b9260a51d0cc057a8aeaf3cb07d9c870
Γιάννης Μικές <mikesgiannis@gmail.com>
2021-06-05 12:05:36 +0300

    Added light and washing machine on/off effect.

--- Smart_Home_management/BathroomPage.xaml.cs
+++ Smart_Home_management/BathroomPage.xaml.cs
@@ -31,18 +31,22 @@ namespace Smart_Home_management {
 
 		private void LightOn(object sender, RoutedEventArgs e) {
 			Light.Foreground = new SolidColorBrush(Colors.LightGreen);
+			LightRect.Visibility = Visibility.Visible;
 		}
 
 		private void LightOff(object sender, RoutedEventArgs e) {
 			Light.Foreground = new SolidColorBrush(Colors.Black);
+			LightRect.Visibility = Visibility.Hidden;
 		}
 
 		private void WmOn(object sender, RoutedEventArgs e) {
 			WashingM.Foreground = new SolidColorBrush(Colors.LightGreen);
+			WmRect.Visibility = Visibility.Visible;
 		}
 
 		private void WmOff(object sender, RoutedEventArgs e) {
 			WashingM.Foreground = new SolidColorBrush(Colors.Black);
+			WmRect.Visibility = Visibility.Hidden;
 		}
 	}
 }
