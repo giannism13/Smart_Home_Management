7ce66c6b9554e4f87b5e9e505400eb48b813e9ce
Γιάννης Μικές <mikesgiannis@gmail.com>
2021-07-12 10:50:35 +0300

    cleanup

--- Smart_Home_management/loginPage.xaml.cs
+++ Smart_Home_management/loginPage.xaml.cs
@@ -12,51 +12,25 @@ namespace Smart_Home_management {
 			InitializeComponent();
 		}
 
-		//private void UsernameField_GotFocus(object sender, RoutedEventArgs e) {
-		//	if (usernameField.Text == " Username ") {
-		//		usernameField.Text = string.Empty;
-		//		usernameField.Foreground = new SolidColorBrush(Colors.Black);
-		//	}
-		//}
-
-		//private void UsernameField_LostFocus(object sender, RoutedEventArgs e) {
-		//	if (usernameField.Text == string.Empty) {
-		//		usernameField.Text = " Username ";
-		//		usernameField.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFA08787"));
-		//	}
-		//}
-
-		//private void PasswordField_GotFocus(object sender, RoutedEventArgs e) {
-		//	if (passwordField.Text == " Password ") {
-		//		passwordField.Text = string.Empty;
-		//		passwordField.Foreground = new SolidColorBrush(Colors.Black);
-		//	}
-		//}
-
-		//private void PasswordField_LostFocus(object sender, RoutedEventArgs e) {
-		//	if (passwordField.Text == string.Empty) {
-		//		passwordField.Text = " Password ";
-		//		passwordField.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FFA08787"));
-		//	}
-		//}
-
 		private void LoginButton_MouseEnter(object sender, MouseEventArgs e) {
-			loginbutton.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF4C70"));
+			loginbutton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF4C70");
 		}
 
 		private void LoginButton_MouseLeave(object sender, MouseEventArgs e) {
-			loginbutton.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FE6584"));
+			loginbutton.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FE6584");
 		}
 
 		private void LoginButton_Click(object sender, RoutedEventArgs e) {
-			if (usernameField.Text == " Username " || passwordField.Password.ToString() == "")
+			if (usernameField.Text == "" || passwordField.Password == "") {
 				errorText.Text = "Username and password fields cannot be empty!";
+			}
 			else if (usernameField.Text == "user" && passwordField.Password.ToString() == "123") {
 				MainWindow objMainWindows = (MainWindow)Window.GetWindow(this);
 				objMainWindows.Main.Navigate(new HomePage(this));
 			}
-			else
+			else {
 				errorText.Text = "Wrong username or password!";
+			}
 		}
 	}
 }
